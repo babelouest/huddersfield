ab2235fbb9014d7d6e65d7a2eb2bdb33d890c616
Nicolas Mora <github@babelouest.org>
2020-02-02 11:29:26 -0500

    Add build/ folder to separate build and test

--- glewlwyd/test/tgz/entrypoint.sh
+++ glewlwyd/test/tgz/entrypoint.sh
@@ -21,7 +21,11 @@ if [ -f $GLEWLWYD_ARCHIVE ]; then
 
   tar -xf $GLEWLWYD_ARCHIVE -C /opt/glewlwyd --strip 1
 
-  cd /opt/glewlwyd/test
+  mkdir /opt/glewlwyd/build
+
+  cd /opt/glewlwyd/build
+
+  cmake -DBUILD_GLEWLWYD_TESTING=on ..
 
   sqlite3 /tmp/glewlwyd.db < /opt/glewlwyd/docs/database/init.sqlite3.sql
 
@@ -30,14 +34,18 @@ if [ -f $GLEWLWYD_ARCHIVE ]; then
   glewlwyd --config-file=/opt/glewlwyd/test/glewlwyd-ci.conf &
 
   export G_PID=$!
+  
+  ../test/cert/create-cert.sh
 
+  ln -s ../test/cert/ .
+
+  ln -s ../test/ .
+  
   make test
   
   kill $G_PID
 
   make glewlwyd_scheme_certificate
-  
-  ../test/cert/create-cert.sh
   
   glewlwyd --config-file=cert/glewlwyd-cert-ci.conf &
 
@@ -45,10 +53,30 @@ if [ -f $GLEWLWYD_ARCHIVE ]; then
   
   sleep 2
   
-  make test_glewlwyd_scheme_certificate || (cat /tmp/glewlwyd-https.log && false)
+  ./glewlwyd_scheme_certificate || (cat /tmp/glewlwyd-https.log && false)
   
   kill $G_PID
-  
+
+  glewlwyd --config-file=test/glewlwyd-profile-delete-disable.conf &
+
+  export G_PID=$!
+
+  sleep 2
+
+  ./glewlwyd_profile_delete disable || (cat /tmp/glewlwyd-disable.log && false)
+
+  kill $G_PID
+
+  glewlwyd --config-file=test/glewlwyd-profile-delete-yes.conf &
+
+  export G_PID=$!
+
+  sleep 2
+
+  ./glewlwyd_profile_delete delete || (cat /tmp/glewlwyd-delete.log && false)
+
+  kill $G_PID
+
   echo "$(date -R) glewlwyd-dev_${GLEWLWYD_VERSION}_`grep -e "^ID=" /etc/os-release |cut -c 4-`_`grep -e "^VERSION_ID=" /etc/os-release |cut -c 12-`_`uname -m`.tar.gz test complete success" >> /share/summary.log
 
 else
